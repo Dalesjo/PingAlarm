6b3858d0d4f8728bc6b9daf9b23cf1c2423290c5
Samuel Dalesjö <samuel@dalesjo.com>
2023-06-25 15:08:01 +0200

    add installation script

---
+++ PingAlarm/install.sh
@@ -0,0 +1,34 @@
+﻿#!/bin/bash
+
+SCRIPTDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+EXECUTABLE=${SCRIPTDIR}/PingAlarm;
+USERNAME=`id -un`
+GROUPNAME=`id -gn`
+
+echo "test"
+
+echo $SCRIPTDIR
+echo $EXECUTABLE
+
+chmod +x $EXECUTABLE
+
+cat << EOF | sudo tee /etc/systemd/system/PingAlarm.service
+[Unit]
+Description="PingAlarm from  https://github.com/Dalesjo/PingAlarm"
+
+[Service]
+User=${USERNAME}
+Group=${GROUPNAME}
+Type=notify
+WorkingDirectory=${SCRIPTDIR}
+ExecStart=${EXECUTABLE}
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable PingAlarm
+sudo systemctl start PingAlarm
+sudo systemctl status PingAlarm
