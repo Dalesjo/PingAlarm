af49df57225071159141a56dcdd8be72aa45131e
Samuel Dalesjö <samuel@dalesjo.com>
2023-06-25 16:04:58 +0200

    update build script

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 if output=$(git status --porcelain) && [ -n "$output" ]; then
   echo "You have uncommited changes"
-  exit 3
+  #exit 3
 fi
 
 # New Version number
@@ -15,14 +15,15 @@ dotnet version --skip-vcs -f ./PingAlarm/PingAlarm.csproj patch
 VERSION=$(cat ./PingAlarm/PingAlarm.csproj | grep -oPm1 "(?<=<Version>)[^<]+" | tr -d '\n')
 echo "New verison is ${VERSION}"
 
-dotnet publish -c Release PingAlarm/PingAlarm.csproj
+dotnet publish -c Release --self-contained --runtime linux-arm64 PingAlarm/PingAlarm.csproj
 
 echo "Commiting changes"
 git add ./PingAlarm/PingAlarm.csproj
+cd PingAlarm/bin/Release/net6.0/linux-arm64/publish/
+rm appsettings.Development.json
+tar -czvf ./../../../../../../images/PingAlarm-${VERSION}.tar.gz *
 
-cd PingAlarm/bin/Release/net6.0/publish/
-tar -czvf ./../../../../../images/PingAlarm-${VERSION}.tar.gz *
-cd ./../../../../../
+cd ./../../../../../../
 
-git commit -m "Built new Version ${VERSION}"
-git push
+#git commit -m "Built new Version ${VERSION}"
+#git push
