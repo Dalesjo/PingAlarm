878999afb9b0bff689611cd574da235af766d228
Samuel Dalesjö <samuel@dalesjo.com>
2023-06-25 15:35:46 +0200

    create build script

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if output=$(git status --porcelain) && [ -n "$output" ]; then
+  echo "You have uncommited changes"
+  exit 3
+fi
+
+# New Version number
+git pull
+
+# dotnet tool install -g dotnet-version-cli
+# https://github.com/skarpdev/dotnet-version-cli
+dotnet version --skip-vcs -f ./PingAlarm/PingAlarm.csproj patch
+
+VERSION=$(cat ./PingAlarm/PingAlarm.csproj | grep -oPm1 "(?<=<Version>)[^<]+" | tr -d '\n')
+echo "New verison is ${VERSION}"
+
+dotnet publish -c Release PingAlarm/PingAlarm.csproj
+
+echo "Commiting changes"
+git add ./PingAlarm/PingAlarm.csproj
+
+cd ./../../../../../
+cd PingAlarm/bin/Release/net6.0/linux-arm64/publish/
+tar -czvf ./../../../../../../images/PingAlarm-${VERSION}.tar.gz *
+cd ./../../../../../../
+
+git commit -m "Built new Version ${VERSION}"
+git push
